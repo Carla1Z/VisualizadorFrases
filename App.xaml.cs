2c1d030fe1deaa58046a65db045185fef832b380
Carla1Z <carla_sabrina_zarate@hotmail.com>
2024-03-09 23:03:45 -0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VisualizadorFrases;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VisualizadorFrases;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
